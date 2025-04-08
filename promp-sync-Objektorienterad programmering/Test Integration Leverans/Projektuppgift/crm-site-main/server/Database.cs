58589e5bb8ee35dd17474809b9fdaa021c554d05
Editor Jakupi <da.editoor@msn.com>
2025-04-08 09:59:49 +0200

    Commited the ZIP file

---
+++ promp-sync-Objektorienterad programmering/Test Integration Leverans/Projektuppgift/crm-site-main/server/Database.cs
@@ -0,0 +1,33 @@
+﻿using server.Enums;
+
+namespace server;
+
+using Npgsql;
+
+public class Database
+{
+
+    private readonly string _host = "localhost";
+    private readonly string _port = "5544";
+    private readonly string _username = "postgres";
+    private readonly string _password = "password";
+    private readonly string _database = "crm-site";
+
+    private NpgsqlDataSource _connection;
+
+    
+    public NpgsqlDataSource Connection()
+    {
+        return _connection;
+    }
+
+    public Database()
+    {
+        string connectionString = $"Host={_host};Port={_port};Username={_username};Password={_password};Database={_database}";
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
+        dataSourceBuilder.MapEnum<Role>("role");
+        dataSourceBuilder.MapEnum<IssueState>("issue_state");
+        dataSourceBuilder.MapEnum<Sender>("sender");
+        _connection = dataSourceBuilder.Build();
+    }
+}
